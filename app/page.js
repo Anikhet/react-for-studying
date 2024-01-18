ce785e40547155c5f91068d9d53b46c4873a9212
Anikhet Mulky <am9559@rit.edu>
2024-01-17 21:32:20 -0800

    adding-tailwind

--- app/page.js
+++ app/page.js
@@ -3,7 +3,10 @@ import React from 'react'
 const page = () => {
   return (
     <>
-    <h1 className='bg-black' text>To do list</h1>
+    <h1 className='bg-black text-white font-bold text-center p-10  text-5xl'>To do list</h1>
+    <form> 
+    <input placeholder='What do you wanna do ?'className=' align-middle'></input>
+    </form>
     </>
   )
 }
